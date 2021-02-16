8be4aee70c7508c896ebbad04c64d659c260dadf
Agustin <gustindvr@gmail.com>
2021-02-15 21:18:32 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primer aplicacion con ReactJs y ElectronJs
         </p>
         <a
           className="App-link"
