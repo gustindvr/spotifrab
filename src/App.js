3c22137680d90da0dd28c6ca49ba213bfbe9bd22
Agustin <gustindvr@gmail.com>
2021-02-16 22:21:44 -0300

    all dependencies instaled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firebase from "./utils/firebase";
+import "firebase/auth";
+import Auth from "./pages/Auth";
+
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  firebase.auth().onAuthStateChanged(currentUser => {
+    if(!currentUser){
+      setUser(null)
+    }else {
+      setUser(currentUser)
+    }
+    setIsLoading(false);
+  });
+  
+  if(isLoading){
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Primer aplicacion con ReactJs y ElectronJs
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    !user ? <Auth /> : <UserLogged />
   );
 }
+
+function UserLogged(){
+
+  const logout = () => {
+    firebase.auth().signOut();
+  }
+
+  return (
+    <div style={{ display: "flex", alignItems:"center", justifyContent:"center", flexDirection:"column", height: "100vh" }}>
+      <h1>Usuario Logueado</h1>
+      <button onClick={ logout}>Cerrar Sesión</button>
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxtIJD57NmOGOWagxIKjHCU7FROY_KH1M",
+  authDomain: "spotifrab.firebaseapp.com",
+  projectId: "spotifrab",
+  storageBucket: "spotifrab.appspot.com",
+  messagingSenderId: "333094966163",
+  appId: "1:333094966163:web:2c724686bdf96889ad8e66"
+};
+
+export default firebase.initializeApp(firebaseConfig);
